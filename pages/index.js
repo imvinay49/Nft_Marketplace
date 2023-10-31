77254294853dfa18b941e7e3abef70e8ad676b3c
imvinay49 <workvinay4912@gmail.com>
2023-10-31 20:06:44 +0530

    change in hello text with ji

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-green-500 text-lg">Hello World hello bhai</h1>
+      <h1 className="text-green-500 text-lg">Hello World hello ji</h1>
     </div>
   )
 }
