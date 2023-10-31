7852ee5406eeec30aa60e2e489641432e300dabd
imvinay49 <workvinay4912@gmail.com>
2023-10-31 20:00:19 +0530

    changed in hello text

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-green-500 text-lg">Hello World</h1>
+      <h1 className="text-green-500 text-lg">Hello World hello vinay</h1>
     </div>
   )
 }
